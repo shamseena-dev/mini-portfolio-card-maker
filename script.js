2ba755b99a6b99d2bfbf9371f8bf86c18041437d
shamseena-dev <shamseenakarumarot@gmail.com>
2019-11-11 20:13:29 +0100

    added validation, cahnged card style,changed color theme

--- script.js
+++ script.js
@@ -1,26 +1,159 @@
-//Define variables for form 
-const name = document.querySelector(".form-group1");
-const email = document.querySelector(".form-group2");
-const role = document.querySelector(".form-group3");
-const description = document.querySelector(".form-group4");
-const linkedIn = document.querySelector(".form-group5");
-const phone = document.querySelector(".form-group6");
-const place = document.querySelector(".form-group7");
-const profilePicture = document.querySelector(".form-group8");
+ //Define variables for form control 
+ const form = document.querySelector(".form1");
+const name = document.querySelector(".form-control1");
+const email = document.querySelector(".form-control2");
+const role = document.querySelector(".form-control3");
+const description = document.querySelector(".form-control4");
+const linkedIn = document.querySelector(".form-control5");
+const phone = document.querySelector(".form-control6");
+const place = document.querySelector(".form-control7");
+const profilePicture = document.querySelector(".form-control8");
+//Define variables for Error messages
+const msg1 = document.querySelector(".message1");
+const msg2 = document.querySelector(".message2");
+const msg3 = document.querySelector(".message3");
+const msg4 = document.querySelector(".message4");
 
-// Define variable for card maker
-const cardName = document.querySelector(".card-name");
-const cardEmail = document.querySelector(".card-email");
-const cardRole = document.querySelector(".card-role");
-const cardDescription = document.querySelector(".card-description");
-const cardLinkedIn = document.querySelector(".card-linkedIn");
-const cardPhone = document.querySelector(".card-phone");
-const cardPlace = document.querySelector(".card-place");
-const cardPic = document.querySelector(".card-pic");
+//Regular Expression for Email validation
+var regEx = /\S+@\S+\.\S+/;
+//Regular Expression for Phone number vaildation
+var regExPhn = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;  
+
+//Define Variable for portfolio card
+const ulList = document.querySelector(".list-group");
+//Add event Listener to form when clicked on Submit
+form.addEventListener('submit',updateDetails);
 
 
-loadEventListeners();
-function loadEventListeners(){
+function updateDetails(e){
+//if name input is null or empty
+if(name.value == "" || name.value == null){
+
+  msg1.innerHTML = '<p> Please enter a name </p>';
+  e.preventDefault(); 
+  
+}
+//if email doesnt match the reg expr pattern
+else if(!(regEx.test(email.value))){
+     msg1.style.display = 'none';
+     msg2.innerHTML = '<p> Please enter a valid email </p>';
+     e.preventDefault(); 
+
+}
+//if role is empty
+else if(role.value == "" || role.value == null){
+    msg1.style.display = 'none';
+    msg2.style.display = 'none';
+    msg3.innerHTML = '<p> Please enter your Role </p>';
+     e.preventDefault(); 
+
+}
+//if phone num is empty or null or doesnt match the reg expr pattern
+else if(phone.value == null || phone.value =="" || regExPhn.test(phone.value) == false){
+ 
+    msg1.style.display = 'none';
+    msg2.style.display = 'none';
+    msg3.style.display = 'none';
+    msg4.innerHTML = '<p> Please enter valid Phone Number </p>';
+    e.preventDefault(); 
+}
+
+//Else create portfolio card and update the details  
+else{
+
+  //Hide all Error messages 
+  msg1.style.display = 'none';
+  msg2.style.display = 'none';
+  msg3.style.display = 'none';
+  msg4.style.display = 'none';
+  // Update Name
+  const li = document.createElement('li');
+  console.log(name.value.toUpperCase());
+  const nameCaps = name.value.toUpperCase();
+
+  li.appendChild(document.createTextNode(nameCaps));
+  li.className = 'list1';
+  ulList.appendChild(li);
+
+  //Update Role
+  const li2 = document.createElement('li');
+  li2.appendChild(document.createTextNode(`Profession : ${role.value}`));
+  li2.className = 'list1';
+  ulList.appendChild(li2);
+
+  //Update Description of role
+  if(description.value != null && description.value !=""){
+      const li3 = document.createElement('li');
+      li3.appendChild(document.createTextNode(`Description : ${description.value}`));
+      li3.className = 'list1';
+      ulList.appendChild(li3);
+      e.preventDefault(); 
+  }
+  //Update LinkedIn
+  if(linkedIn.value != null && linkedIn.value !=""){
+      const li4 = document.createElement('li');
+      li4.appendChild(document.createTextNode(`LinkedIn : ${linkedIn.value}`));
+      li4.className = 'list1';
+      ulList.appendChild(li4);
+       e.preventDefault(); 
+   }
+  
+  //Update Email
+  const li5 = document.createElement('li');
+  li5.appendChild(document.createTextNode(`Email : ${email.value}`));
+  li5.className = 'list1';
+  ulList.appendChild(li5);
+
+  //Update phn
+  const li6 = document.createElement('li');
+  li6.appendChild(document.createTextNode(`Phn : ${phone.value}`));
+  li6.className = 'list1';
+  ulList.appendChild(li6);
+
+  //Update Place
+  if(place.value != null && place.value !=""){
+      const li7 = document.createElement('li');
+      li7.appendChild(document.createTextNode(`Place : ${place.value}`));
+      li7.className = 'list1';
+      ulList.appendChild(li7);
+       e.preventDefault(); 
+   }
+   //Finally, Clear all input values
+    name.value = '';  
+    email.value = '';
+    role.value = '';
+    description.value = '';
+    linkedIn.value = '';
+    phone.value = '';
+    place.value = '';
+    profilePicture.value = '';
+    e.preventDefault(); 
+
+}
+}
+
+
+//To update profile pic
+var openFile = function(file) {
+    var input = file.target;
+
+    var reader = new FileReader();
+    reader.onload = function(){
+      var dataURL = reader.result;
+      var output = document.querySelector('.card-pic');
+      output.src = dataURL;
+    };
+    reader.readAsDataURL(input.files[0]);
+  };
+
+
+
+
+
+
+
+
+/*
     name.addEventListener('input', updateName);
     
     email.addEventListener('input', updateEmail);
@@ -34,9 +167,6 @@ function loadEventListeners(){
 
 }
 
-function check(e){
-	console.log(e.target.value);
-};
 
 function updateName(e){
 	cardName.innerHTML =`My Name is ${e.target.value}`;
@@ -66,6 +196,7 @@ function updatePlace(e){
 	cardPlace.innerHTML =`Place : ${e.target.value}`;
 
 }
+*/
 
 //To update profile pic
 var openFile = function(file) {
@@ -79,3 +210,4 @@ var openFile = function(file) {
     };
     reader.readAsDataURL(input.files[0]);
   };
+
